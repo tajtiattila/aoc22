25fb7228861d7acbe928c318c77df764da24c7c3
Attila Tajti <attila.tajti@gmail.com>
2022-12-21 10:27:11 +0100

    21/2

--- src/day21.rs
+++ src/day21.rs
@@ -4,26 +4,114 @@ use std::fmt;
 
 pub fn run(input: &str) -> Result<String> {
     let m = parse(input)?;
-    let p1 = eval(&m, Monkey::parse("root"))?;
-    let p2 = "";
+    let p1 = Eval::new(&m).root()?;
+    let p2 = find_yell(&m)?;
     Ok(format!("{} {}", p1, p2))
+}
+
+fn find_yell(m: &MonkeyMap) -> Result<Num> {
+    find_zero(0, |x| Eval::with_human(m, x).root().unwrap())
+        .ok_or_else(|| anyhow!("can't find what to yell"))
+}
+
+fn find_zero<FN>(start: Num, mut f: FN) -> Option<Num>
+where
+    FN: FnMut(Num) -> Num,
+{
+    let mut p = (start, f(start));
+    let mut q = (0..1_000_000).map(|x| (x, f(x))).find(|(_, y)| *y != p.1)?;
+    let mut dx = q.0 - p.0;
+    let dir = sign(0 - p.0) - sign(q.1 - p.1);
+
+    while sign(p.1) * sign(q.1) > 0 {
+        let x = q.0 + dir * dx;
+        p = q;
+        q = (x, f(x));
+        dx *= 2;
+    }
+
+    loop {
+        if p.0 + 1 == q.0 {
+            return None;
+        }
+
+        let x = (p.0 + q.0) / 2;
+        let m = (x, f(x));
+        if m.1 == 0 {
+            return Some(m.0);
+        }
+
+        if sign(p.1) * sign(m.1) > 0 {
+            p = m;
+        } else {
+            q = m;
+        }
+    }
+}
+
+fn sign(x: Num) -> Num {
+    if x != 0 {
+        if x < 0 {
+            -1
+        } else {
+            1
+        }
+    } else {
+        0
+    }
 }
 
 type MonkeyMap = HashMap<Monkey, Yell>;
 
-fn eval(m: &MonkeyMap, mky: Monkey) -> Result<Num> {
-    Ok(
-        match m
+struct Eval<'a> {
+    m: &'a MonkeyMap,
+    humn: Option<Num>,
+}
+
+static ROOT: Monkey = Monkey::from_chars('r', 'o', 'o', 't');
+static HUMN: Monkey = Monkey::from_chars('h', 'u', 'm', 'n');
+
+impl<'a> Eval<'a> {
+    fn new(m: &'a MonkeyMap) -> Self {
+        Self { m, humn: None }
+    }
+
+    fn with_human(m: &'a MonkeyMap, humn: Num) -> Self {
+        Self {
+            m,
+            humn: Some(humn),
+        }
+    }
+
+    fn root(&self) -> Result<Num> {
+        self.eval(ROOT)
+    }
+
+    fn eval(&self, mky: Monkey) -> Result<Num> {
+        let yell = self
+            .m
             .get(&mky)
-            .ok_or_else(|| anyhow!("invalid monkey {}", mky.to_string()))?
-        {
-            Yell::Const(n) => *n,
-            Yell::Add(l, r) => eval(m, *l)? + eval(m, *r)?,
-            Yell::Sub(l, r) => eval(m, *l)? - eval(m, *r)?,
-            Yell::Mul(l, r) => eval(m, *l)? * eval(m, *r)?,
-            Yell::Div(l, r) => eval(m, *l)? / eval(m, *r)?,
-        },
-    )
+            .ok_or_else(|| anyhow!("invalid monkey {}", mky.to_string()))?;
+        Ok(match yell {
+            Yell::Const(x) => {
+                let humn = if mky == HUMN { self.humn } else { None };
+                if let Some(z) = humn {
+                    z
+                } else {
+                    *x
+                }
+            }
+            Yell::Calc(op, l, r) => {
+                let l = self.eval(*l)?;
+                let r = self.eval(*r)?;
+                if mky == ROOT && self.humn.is_some() {
+                    l - r
+                } else {
+                    op.calc(l, r)
+                }
+            }
+        })
+    }
 }
 
 fn parse(input: &str) -> Result<MonkeyMap> {
@@ -34,7 +122,7 @@ fn parse(input: &str) -> Result<MonkeyMap> {
             let errf = || anyhow!("parsing error on line {}: {}", i + 1, line);
             let (m, r) = line.split_once(": ").ok_or_else(errf)?;
             Yell::parse(r)
-                .map(|y| (Monkey::parse(m), y))
+                .map(|y| (Monkey::from(m), y))
                 .ok_or_else(errf)
         })
         .collect()
@@ -44,12 +132,16 @@ fn parse(input: &str) -> Result<MonkeyMap> {
 struct Monkey(u32);
 
 impl Monkey {
-    fn parse(s: &str) -> Monkey {
+    fn from(s: &str) -> Monkey {
         Monkey(
             s.chars()
                 .map(|c| (c as u32))
                 .fold(0, |acc, x| (acc << 8) | x),
         )
+    }
+
+    const fn from_chars(a: char, b: char, c: char, d: char) -> Monkey {
+        Monkey(((a as u32) << 24) | ((b as u32) << 16) | ((c as u32) << 8) | (d as u32))
     }
 
     fn as_string(&self) -> String {
@@ -71,10 +163,26 @@ type Num = i64;
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 enum Yell {
     Const(Num),
-    Add(Monkey, Monkey),
-    Sub(Monkey, Monkey),
-    Mul(Monkey, Monkey),
-    Div(Monkey, Monkey),
+    Calc(Op, Monkey, Monkey),
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    fn calc(self, l: Num, r: Num) -> Num {
+        match self {
+            Self::Add => l + r,
+            Self::Sub => l - r,
+            Self::Mul => l * r,
+            Self::Div => l / r,
+        }
+    }
 }
 
 impl Yell {
@@ -84,14 +192,14 @@ impl Yell {
             return Some(Yell::Const(x));
         }
         let mut it = s.split(' ');
-        let lm = it.next().map(Monkey::parse)?;
+        let lm = it.next().map(Monkey::from)?;
         let op = it.next()?.chars().next()?;
-        let rm = it.next().map(Monkey::parse)?;
+        let rm = it.next().map(Monkey::from)?;
         match op {
-            '+' => Some(Yell::Add(lm, rm)),
-            '-' => Some(Yell::Sub(lm, rm)),
-            '*' => Some(Yell::Mul(lm, rm)),
-            '/' => Some(Yell::Div(lm, rm)),
+            '+' => Some(Yell::Calc(Op::Add, lm, rm)),
+            '-' => Some(Yell::Calc(Op::Sub, lm, rm)),
+            '*' => Some(Yell::Calc(Op::Mul, lm, rm)),
+            '/' => Some(Yell::Calc(Op::Div, lm, rm)),
             _ => None,
         }
     }
